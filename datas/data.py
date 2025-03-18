4214cef6229ca6d448191de7c4e20d4616fa187b
MiYinWt <330324wutao@gmail.com>
2025-03-18 17:25:39 +0800

    changes

--- datas/data.py
+++ datas/data.py
@@ -1,7 +1,7 @@
-import torch_scatter
 import torch
+import torch_scatter
 import numpy as np
-from torch_geometric.data import Data, Batch
+from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 
 FOLLOW_BATCH = ('protein_element', 'ligand_element', 'ligand_bond_type',)
@@ -29,20 +29,11 @@ class ProteinLigandData(Data):
                                        for i in instance.ligand_bond_index[0]}
         return instance
 
-    # def __inc__(self, key, value, *args, **kwargs):
-    #     if key == 'ligand_bond_index':
-    #         return self['ligand_element'].size(0)
-    #     else:
-    #         return super().__inc__(key, value)
     def __inc__(self, key, value, *args, **kwargs):
-        if key == 'bond_index':
-            return len(self['node_type'])
-        elif key == 'edge_index':
-            return len(self['node_type'])
-        elif key == 'halfedge_index':
-            return len(self['node_type'])
+        if key == 'ligand_bond_index':
+            return self['ligand_element'].size(0)
         else:
-            return super().__inc__(key, value, *args, **kwargs)
+            return super().__inc__(key, value)
 
 
 class ProteinLigandDataLoader(DataLoader):
@@ -66,3 +57,26 @@ def torchify_dict(data):
         else:
             output[k] = v
     return output
+
+
+def get_batch_connectivity_matrix(ligand_batch, ligand_bond_index, ligand_bond_type, ligand_bond_batch):
+    batch_ligand_size = torch_scatter.segment_coo(
+        torch.ones_like(ligand_batch),
+        ligand_batch,
+        reduce='sum',
+    )
+    batch_index_offset = torch.cumsum(batch_ligand_size, 0) - batch_ligand_size
+    batch_size = len(batch_index_offset)
+    batch_connectivity_matrix = []
+    for batch_index in range(batch_size):
+        start_index, end_index = ligand_bond_index[:, ligand_bond_batch == batch_index]
+        start_index -= batch_index_offset[batch_index]
+        end_index -= batch_index_offset[batch_index]
+        bond_type = ligand_bond_type[ligand_bond_batch == batch_index]
+        # NxN connectivity matrix where 0 means no connection and 1/2/3/4 means single/double/triple/aromatic bonds.
+        connectivity_matrix = torch.zeros(batch_ligand_size[batch_index], batch_ligand_size[batch_index],
+                                          dtype=torch.int)
+        for s, e, t in zip(start_index, end_index, bond_type):
+            connectivity_matrix[s, e] = connectivity_matrix[e, s] = t
+        batch_connectivity_matrix.append(connectivity_matrix)
+    return batch_connectivity_matrix
